c0416765c83a06b458776d5c51814d81d5d55d9d
Kwok Yan Lam <90571342+yan1100@users.noreply.github.com>
2021-09-18 23:47:27 -0700

    Add files via upload

---
+++ Assignment 1/Question 1/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static void sortArray(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = 0;
+        while (index <= right){
+            if (nums[index] == 0){
+                swap(nums,index,left);
+                left++;
+                index++;
+            }else if (nums[index] == 1){
+                index++;
+            }else {
+                swap(nums,index,right);
+                right--;
+            }
+        }
+    }
+
+    public static void swap (int[] list, int index1, int index2){
+        int temp = list[index1];
+        list[index1] = list[index2];
+        list[index2] = temp;
+    }
+}
